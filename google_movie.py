3a65618991fc48dd49643f23feff598059b23e78
David <davidyen1124@gmail.com>
2013-11-03 20:44:25 +0800

    First push

---
+++ google_movie.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+class Movie:
+    def __init__(self, hl='en'):
+        self.base_url = 'http://www.google.com'
+        self.hl = hl
+
+    def searchByCoordinates(self, latitude, longitude):
+        '''Latitude and longitude should be double.
+           Like (25.0333, 121.63333)'''
+
+        self.near = '{0},{1}'.format(latitude, longitude)
+
+    def searchByLocation(self, location):
+        '''The place name of the theater.'''
+
+        self.near = location
+
+    def __makeUrl(self, sort=None, start=None, mid=None):
+        url = self.base_url + '/movies?'
+        url += 'hl={0}'.format(self.hl)
+        url += '&near={0}'.format(self.near)
+
+        if sort is None:
+            url += '&sort=1'
+        else:
+            url += '&sort={0}'.format(sort)
+
+        if start is not None:
+            url += '&start={0}'.format(start)
+
+        if mid is not None:
+            url += '&mid={0}'.format(mid)
+
+        return url
+
+    def __getPageIndexes(self):
+        '''Retrieve all page indexes from the first page.'''
+
+        url = self.__makeUrl()
+        pages = []
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content)
+        navbar = soup.find('div', id='navbar')
+
+        if navbar is None:
+            return pages
+
+        for td in navbar.find_all('td'):
+            text = td.get_text()
+            try:
+                page = int(text)
+                pages.append(page)
+            except ValueError:
+                pass
+        return pages
+
+    def __getMovieLinkInOnePage(self, start):
+        '''Get all the links of movies in the specifc page.'''
+
+        url = self.__makeUrl(start=start)
+        links = []
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content)
+        for title in soup.find_all('h2', itemprop='name'):
+            a = title.find('a')
+            if a:
+                links.append(self.base_url + a['href'])
+        return links
+
+    def getMovieLinks(self):
+        '''Get all the movie links in every page.'''
+
+        links = []
+        pages = self.__getPageIndexes()
+        for page in pages:
+            page = (page - 1) * 10
+            page_links = self.__getMovieLinkInOnePage(page)
+            links.extend(page_links)
+        return links
+
+    def __getMovieId(self, url):
+        mid = ''
+        for segment in url.split('&'):
+            if 'mid' in segment:
+                mid = segment.split('=')[1]
+        return mid
+
+    def getMovieDetail(self, url):
+        mid = self.__getMovieId(url)
+        url = self.__makeUrl(mid=mid)
+
+        time_re = re.compile(r' &nbsp.*?(\d{2}:\d{2})')
+        theaters = []
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content)
+        movie_name = soup.find('h2', itemprop='name').get_text()
+
+        movie_description = ''
+        for text in soup.find('div', class_='syn').stripped_strings:
+            if u'more' not in text and u'less' not in text:
+                movie_description += text
+
+        infos = soup.find('div', class_='info').get_text()
+        movie_info = [info.strip() for info in infos.split('-')]
+
+        movie_length = ''
+        movie_genre = ''
+        movie_language = ''
+        movie_subtitle = ''
+        movie_actors = ''
+        for counter, text in enumerate(movie_info):
+            if counter == 0:
+                movie_length = text
+            elif counter == 1:
+                movie_genre = text
+            elif counter == 2:
+                movie_language = text
+            elif counter == 3:
+                movie_subtitle = text
+            elif counter == 4:
+                movie_actors = text
+
+        for theater in soup.find_all('div', class_='theater'):
+            theater_name = theater.find('div', class_='name').get_text()
+            theater_address = theater.find('div', class_='address').get_text()
+
+
+            theater_times = []
+            time = theater.find('div', class_='times')
+            for span in time.find_all('span'):
+                time_match = time_re.search(span.get_text())
+                if time_match:
+                    theater_times.append(time_match.group(1))
+            theaters.append({'theater_name': theater_name,
+                             'theater_address': theater_address,
+                             'theater_times': theater_times})
+
+        return {'movie_name': movie_name,
+                'movie_description': movie_description,
+                'movie_length': movie_length,
+                'movie_genre': movie_genre,
+                'movie_language': movie_language,
+                'movie_subtitle': movie_subtitle,
+                'movie_actors': movie_actors,
+                'theater': theaters}
+
+    def getMovies(self):
+        movies = []
+
+        links = self.getMovieLinks()
+        for link in links:
+            movie = self.getMovieDetail(link)
+            movies.append(movie)
+        return movies
+
+
+def main():
+    movie = Movie(hl='zh-TW')
+
+    # Search by the latitude and longitude
+    movie.searchByCoordinates(25.0333, 121.6333)
+    # or search by the location name
+    #movie.searchByLocation('Taipei')
+
+    movies = movie.getMovies()
+
+    # Print all the movie name
+    for movie in movies:
+        print movie['movie_name']
+
+
+if __name__ == '__main__':
+    main()
